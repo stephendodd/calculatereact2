20c9043c4aee2ee63a1d4e84efc72cc360f3d066
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-26 09:38:16 +1000

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+})
+
+module.exports = App
